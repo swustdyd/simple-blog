161a040bbe0f393a5d7678ef02567fed63cecf45
aarondeng <1562044678@qq.com>
2018-09-04 16:17:48 +0800

    添加了获取项目文件中所有路由的方法

--- server/src/utils/util.js
+++ server/src/utils/util.js
@@ -1,4 +1,7 @@
 import crypto from 'crypto'
+import path from 'path'
+import fs from 'fs'
+import logger from '../utils/logger'
 
 /**
  * 对密码进行md5加密
@@ -21,4 +24,48 @@ export function md5Password(password: string, key: string){
  */
 export function comparePassword(password, passwordMd5, key){
     return passwordMd5 === md5Password(password, key);
+}
+
+/**
+ * 从项目文件中获取所有的路由
+ */
+export function getAllRoutes(){
+    // 存储所有的路由配置信息
+    const allRoutes = [];
+
+    // 读取controller文件夹下的文件
+    const dirPath = path.resolve(__dirname, '../controllers');
+    const controllersTmp = fs.readdirSync(dirPath).map((fileName) => {
+        const controller = require(path.join(dirPath, fileName)).default;
+        if(controller && controller._isController){
+            return new controller();
+        }
+    })
+
+    // 过滤掉undefined的项
+    const controllers = controllersTmp.filter((controller) => {
+        return controller !== undefined;
+    })
+
+    // 遍历路由配置
+    controllers.forEach((controller) => {
+        if(controller._routes && controller._routes.length > 0){
+            controller._routes.forEach((route) => {
+                let names = [];
+                if(route.middleware && route.middleware.length > 0){
+                    names = route.middleware.map((item) => {
+                        return item.name;
+                    })
+                }            
+                allRoutes.push({
+                    ...route,
+                    middleware: names.length > 0 ? names : undefined
+                });
+            })
+        }
+    })
+
+    // 将所有的路由配置存储为一个文件
+    // fs.writeFileSync(path.resolve(__dirname, 'routeConfig.json'), JSON.stringify(allRoutes, null, 4))
+    return allRoutes;
 }
