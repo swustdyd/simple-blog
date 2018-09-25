1f374b9c23fc6260eefb1b8acda8bfa757767101
aarondeng <1562044678@qq.com>
2018-09-25 18:06:30 +0800

    解决部分参数初始化，导致无法生成对应type文件的bug

--- src/projectInit/createServiceType.js
+++ src/projectInit/createServiceType.js
@@ -20,12 +20,11 @@ const types = [
 ].join(', ');
 
 function checkTypesForType(type = ''){
-    const reg = new RegExp('[a-zA-Z]*<[a-zA-Z<>]*?>', 'g')
+    const reg =/[a-zA-Z]*<[a-zA-Z<>]*?>/g
     const start = type.indexOf('<');
     const end = type.lastIndexOf('>');
     const preType = type.substring(0, start);
     const newType = type.substring(start + 1, end);
-    logger.info(type, preType, newType);
     if(types.indexOf(preType) === -1){
         extraTypes.push(preType);
     }
@@ -38,21 +37,18 @@ function checkTypesForType(type = ''){
 }
 
 function checkExtraTypesForFile(fileString = ''){
-    const reg = new RegExp('(:\\s*[a-zA-Z<>]*?[,\\)}])|([a-zA-Z]*<[a-zA-Z<>]*>)', 'g')
+    const reg = /(:\s*[a-zA-Z<>]*?[,\)}])|([a-zA-Z]*<[a-zA-Z<>]*>)/g;
     const matchResult = fileString.match(reg);
     if(matchResult){
-        let result = matchResult.map((type) => {
-            type = type.replace(/[:,\)}\s]/g, '');
-            logger.debug(type);
-            if(reg.test(type)){                
+        let result = matchResult.map((originType) => {
+            const type = originType.replace(/[:,\)}\s]/g, '');
+            reg.lastIndex = 0;
+            const isNestingType = reg.test(type);
+            if(isNestingType){           
                 checkTypesForType(type);
-                return '';
             }else if(types.indexOf(type) === -1){
                 return type;
-            }else{
-                return '';
             }
-            
         })
         result = result.filter((type) => { return type})
         extraTypes.push(...result);
@@ -61,25 +57,25 @@ function checkExtraTypesForFile(fileString = ''){
 
 function formatServicePropertys(filePath) {
     const fileString = fs.readFileSync(filePath, {encoding: 'utf8'});
-    const itemReg = new RegExp('\\/\\*\\*[\\s\\S]*?\\*\\/[a-zA-Z()<>:=,\\r\\n\\s]*?{', 'g')
-    const commentReg = new RegExp('\\/\\*\\*[\\s\\S]*?\\*\\/', 'g')
+    const itemReg = /\/\*\*[\s\S]*?\*\/[a-zA-Z(){}<>:=,\?\[\]\r\n\s]*?{\r\n/g;
+    const commentReg = /\/\*\*[\s\S]*?\*\//g
     const propertys = [];
     fileString.match(itemReg) && fileString.match(itemReg).forEach((item) => {  
         const comment = item.match(commentReg)[0].replace(/\r\n/g, '\r\n\t');
         let index = item.lastIndexOf('*/');
         let other = item.substring(index + 2);
-        
-        checkExtraTypesForFile(other);
-
         index = other.indexOf('(');
         const name = other.substring(0, index).replace(/\r\n/g, '').split(/\s/).filter((item) => {return item && item !== 'async'});
         other = other.substring(index).replace(/\r\n/g, '');
-        const params = other.substring(0, other.lastIndexOf(')') + 1);
+        const params = other.substring(0, other.lastIndexOf(')') + 1).replace(/=\s*[^,\)]+(,)?(\))?/g, '$1$2');        
+        checkExtraTypesForFile(params);
         const matchResult = other.match(/\)\s*:/);
         const matchString = matchResult ? matchResult[0] : ''; 
         index = matchResult ? matchResult.index : index;
-        const returns = matchString ? other.substring(index + matchString.length).replace('{', '').replace(/\s/g, '') : 'void';
-        propertys.push(`\t\t${comment}${lineBreak}\t\t${name}: ${params} => ${returns}`)
+        const returns = matchString ? other.substring(index + matchString.length).replace('{', '').replace(/\s/g, '') : 'void';        
+        checkExtraTypesForFile(returns);
+        const itemString = `\t\t${comment}${lineBreak}\t\t${name}: ${params} => ${returns}`
+        propertys.push(itemString)
     })
     return propertys;
 }
