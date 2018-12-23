82c59a89b2a9a2f7a59336163a2793a52391b7e9
dyd <1562044678@qq.com>
2018-12-24 01:59:46 +0800

    ...

---
+++ src/type/service.js
@@ -0,0 +1,34 @@
+export type ServiceType = {
+
+	/** 后台Api服务 */
+	apiService: {
+
+
+	},
+
+	/** 菜单service */
+	menuService: {
+
+
+	},
+
+	roleAndApiService: {
+
+
+	},
+
+	roleAndMenuService: {
+
+
+	},
+
+	roleService: {
+
+
+	},
+
+	userService: {
+
+
+	}
+}
